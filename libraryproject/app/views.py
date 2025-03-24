0b0037c68d09afded2c179b2c261120356ab0b1f
psjadhav19teen <psjadhav19teen@gmail.com>
2025-03-24 09:05:38 +0530

    library ORM+DTL

--- libraryproject/app/views.py
+++ libraryproject/app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Book
 
 # Create your views here.
+
+
+# def index(req):
+#     allbooks = Book.objects.all()
+#     print(allbooks)
+#     return HttpResponse(f"All Books:{allbooks}")
+
+
+def index(req):
+    allbooks = Book.objects.all()
+    print(allbooks)
+    context = {"myname": "ITV"}
+    return render(req, "index.html", context)
